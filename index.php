128a25aac80abb1742a647b47d6e09b23547cb20
m1rl4ne <mirgomes013@gmail.com>
2025-11-11 21:12:25 -0300

    11/11 21:11

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color: blue;
+        }
+body{
+background-color: green;
+        }
+    </style>
+</head>
+<body>
+   <h1> FORMULÁRIO SENAI</h1> 
+   <br>
+<?php
+$nomeFilme = "Titanic";
+$anoLançamento = 1998;
+$precoAluguel = 12.99;
+$foiAssistido = true;
+if($precoAluguel > 15)
+{
+    echo "O preço do alguel está muito alto.";
+}
+else
+{
+    echo "O preço do alguel está ótimo.";
+}
+?>
+</body>
+</html>
